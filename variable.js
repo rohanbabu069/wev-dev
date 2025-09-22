c5ab640746474a3bd62b1f3181898c7a066ad057
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2025-09-22 17:46:11 +0530

    add somthing more

--- variable.js
+++ variable.js
@@ -2,10 +2,14 @@ const accountid = 1233
 let accountemail = "abc@google.com"
 var accountPasaword = "321"
 const b="Rohan"
+const c="Babu"
 accountCity = "Moradabad"
 
 // accountid = 23  constent variable is not change 
 
+
+console.log(b)
+console.log(c)
 console.log(accountid);
 console.log(accountPasaword);
 console.log(accountemail);

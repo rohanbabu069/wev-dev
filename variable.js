e13c910cfd914a572c3f8e8cb05e90986a492e0c
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2025-09-22 17:31:46 +0530

    learn-some-basic

---
+++ variable.js
@@ -0,0 +1,15 @@
+const accountid = 1233
+let accountemail = "abc@google.com"
+var accountPasaword = "321"
+accountCity = "Moradabad"
+
+// accountid = 23  constent variable is not change 
+
+console.log(accountid);
+console.log(accountPasaword);
+console.log(accountemail);
+console.log(accountCity)
+
+// Or second Method use table() it show the result in table form
+
+console.table([accountPasaword,accountemail,accountid,accountCity])

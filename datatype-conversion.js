61f39d06b733054018278e262c841fa3bac34b58
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2025-09-24 16:32:55 +0530

    learn type conversion in javascript

---
+++ datatype-conversion.js
@@ -0,0 +1,53 @@
+let score=34
+console.log(score)
+console.log(typeof score)
+
+let a=36
+let b=String(a)
+console.log(b)
+console.log(typeof b)
+
+let c='Rohan'
+let d= Number(c)
+console.log(d)
+console.log(typeof d)
+
+let e='23abc'
+let f=Number(e)
+console.log(typeof f)
+console.log(f)
+
+let g='23'
+console.log(g)
+console.log(typeof g)
+let h=Number(g)
+console.log(h)
+console.log(typeof h)
+
+let as=1
+console.log(as)
+console.log(typeof as)
+let sa=Boolean(as)
+console.log(sa)
+console.log(typeof sa)
+
+let aq=0
+console.log(aq)
+console.log(typeof aq)
+let qa=Boolean(aq)
+console.log(qa)
+console.log(typeof qa)
+
+let df=null
+console.log(df, typeof df)
+let fd=Boolean(df)
+console.log(fd,typeof fd)
+
+let sd=undefined
+console.log(sd, typeof sd)
+let ds=Boolean(sd)
+console.log(ds,typeof ds)
+
+let xs=23
+let sx=xs.toString()
+console.log(sx,typeof sx)

27e33cbe8993e7f762c7af1847bdfe8c34d99163
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2025-09-24 18:07:02 +0530

    learn basic operation

---
+++ operation.js
@@ -0,0 +1,11 @@
+let a=2*2
+let b=2**2
+let c=2-2
+let d=25%2
+let e=78/4
+// console.table(a,b,c,d,e)
+console.log(a,b,c,d,e)
+console.log(2+'2')
+console.log('2'+3)
+console.log(2+'3'+4)
+console.log(2+'3')

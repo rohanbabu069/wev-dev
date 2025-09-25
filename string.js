50ea1cf620eb08833df723989111f0b89262d4e2
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2025-09-25 19:16:46 +0530

    how to use string and its basic method

---
+++ string.js
@@ -0,0 +1,31 @@
+// let a='Rohan'
+// console.log(a)
+
+// let s=new String('Rohan babu')
+// console.log(s, typeof s)
+
+// const ab=new String('Dishu')
+// console.log(ab, typeof ab)
+
+// const q=`This is a number ${12} and ${21} it is also a number`
+// console.log(q)
+
+// const a='Rohan'
+// console.log(a.length)
+// console.log(a.charAt(3))
+// console.log(a.toLowerCase())
+
+// let a='Rohan Chandravanshi'
+// console.log(a.substring(1,8))
+// console.log(a.slice(-2,3))
+
+// let b='      Rohan        '
+// console.log(b)
+// console.log(b.trim())
+/*the trim() method id use to remove the white space fromm the line  */
+
+// let a="Rohan%Babu"
+// console.log(a.replace('%','-'))
+
+let a='RohanBabuChandravanshi'
+console.log(a.split(''))

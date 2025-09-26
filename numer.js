a439c47c2031d67a6e334dadf7fce1624301fab9
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2025-09-26 19:21:25 +0530

    use some math function

--- numer.js
+++ numer.js
@@ -10,6 +10,15 @@
 // console.log(a.toFixed(2))
 // toFixed()method is use to make fix a number to 2 digit after point
 
-let a=1000000
-console.log(a.toLocaleString())
+// let a=1000000
+// console.log(a.toLocaleString())
 // or we use toLocaleString(en-IN) it means its work  for inndian country
+
+console.log(Math.random());
+console.log((Math.random()*10)+1);
+console.log(Math.floor(Math.random()*10)+1);
+
+
+const min=10
+const max=20
+console.log(Math.floor(Math.random()*(max-min+1)+min))

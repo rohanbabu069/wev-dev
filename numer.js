5c6902407e828a37c49c4e531d7e377ead1089d9
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2025-09-26 19:03:17 +0530

    learn some basic about number

---
+++ numer.js
@@ -0,0 +1,15 @@
+// let a=300
+// console.log(a,typeof a);
+
+// let b= new Number(100)
+// console.log(b,typeof b)
+// c=b.toString()
+// console.log(c,typeof c)
+
+// let a=100
+// console.log(a.toFixed(2))
+// toFixed()method is use to make fix a number to 2 digit after point
+
+let a=1000000
+console.log(a.toLocaleString())
+// or we use toLocaleString(en-IN) it means its work  for inndian country

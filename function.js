764d05075fccac3c3f87dd219be8964dd6f40de9
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2025-10-09 19:52:10 +0530

    learning about function

---
+++ function.js
@@ -0,0 +1,43 @@
+// function SayMyName(){
+//  console.log("R")
+// console.log("O")
+// console.log("H")
+// console.log("A")
+// console.log("N")   
+// }
+
+// SayMyName()
+// SayMyName()
+
+// function sumtwonumber(num1,num2){
+//     console.log(num1+num2)
+// }
+// const b=sumtwonumber(3,6) 
+// console.log(b)
+
+/* by default a rule in javascript programming is that after use of return keyword anything will not be executed*/
+
+// function subtract(num1,num2){
+//     let sub=num1-num2
+//     return sub
+//     console.log("Rohan")
+
+// }
+
+// const c=subtract(9,3)
+// console.log(c)
+
+
+// function SumTwoNumber(num1,num2){
+//     return num1+num2
+// }
+
+// const a=SumTwoNumber(2,3)
+// console.log(a)
+
+function MyName(username){
+return`${username} just logged in`
+}
+
+let a=MyName("Rohan")
+console.log(a)
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,2 +1,10 @@
-(a.push(b))
-// console.log
+function SayMyName(){
+//  console.log("R")
+// console.log("O")
+// console.log("H")
+// console.log("A")
+// console.log("N")   
+// }
+
+// SayMyName()
+// SayMyNa

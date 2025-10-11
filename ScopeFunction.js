6c7f80fc63cb086f9b938aafce83133a6c105a8b
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2025-10-11 18:58:04 +0530

    learn some basic about function

---
+++ ScopeFunction.js
@@ -0,0 +1,44 @@
+// let a=100
+// const b=200
+// var c=300
+// // console.log(a,b,c)
+
+// if(true){
+//     let a='helo'
+//     let b='hii'
+//     var c='bye'
+//     console.log(a,b,c)
+// }
+// console.log(a,b,c)
+
+// const a=function fun(){
+//     let b='Rohan'
+//     console.log(b)
+// }
+// a()
+// fun()
+
+// function one(){
+//     let a='Rohan'
+//     function two(){
+//         let b='Babu'
+//         console.log(a)
+//         console.log(b)
+//     }
+//     two()
+//     // console.log(b)
+// }
+// one()
+// console.log(one(5))
+// function one(pera){
+//     return pera +1
+// }
+// // console.log(one(5))
+
+
+
+// console.log(a(7))
+// const a=function two(num1){
+//     return num1+3
+// }
+/*  Cannot access 'a' before initialization*/

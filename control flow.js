d4103b90ce13d41734cecaa22c278a5711813ade
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2025-10-26 01:52:31 +0530

    learning Basics of control and flow

---
+++ control flow.js
@@ -0,0 +1,130 @@
+// const IsloggedIn=true 
+
+// if (3>2){
+//     console.log('Hello World')
+// }
+/* consditional operator >,<,>=,<=,!(not),!=(not equal to),==(equal to),===(it is also same as equal to but its check type also) exam; if(2=="2") here int is equal to string so If(2=="2") will not give any error*/ 
+
+// let a=2
+// if(a=="2"){
+//     console.log('Rohan')
+// }
+// console.log('Babu')
+
+// if (2=="3"){
+//     console.log('Rohan')
+// }
+// console.log('Babu')
+
+// if (2==='2'){
+//     console.log('rohan')
+// }
+// console.log('babu')
+
+// let a=2
+// if (a>5){
+//     console.log('hello')
+// }
+// else{
+//     console.log('bye')
+// }
+ 
+/*let a=23
+if(a==23) console.log('hii'),console.log('hlo')
+ this is called explicitly execution */ 
+
+// const money = 1500
+// if (money<600){
+//     console.log("Money is not enough")
+// }
+// else if(money<800){
+//     console.log("Money is not enough")
+// }
+// else if(money<1500){
+//     console.log("Money is not enough")
+// }
+// else{
+//     console.log("money is enough")
+// }
+
+// -------------------switch case------------------
+
+// switch(key){
+//     case value:
+
+//         break;
+//     default:
+//         break
+// }
+
+// const a=5
+
+// switch (a){
+//     case (1):
+//         console.log("Jan");
+//         break;
+//     case(2):
+//         console.log("Feb");
+//         break;
+//     case(3):
+//         console.log("March");
+//         break;
+//     case(4):
+//         console.log("April");
+//         break;
+//     case(5):
+//         console.log("May")
+//         break;
+//     default:
+//         console.log("case does not match")
+// }
+
+
+// ---------------false value---------
+
+// false, -0, 0, "", BigInt 0n, null, undefined, NaN
+
+// ---------------truthy value---------
+
+// [], "0", "false", {}, function(){}, "  ", etc.
+
+// const userloggedin= "RB@gamil.com"
+//  if(userloggedin){
+//     console.log("user is logged in.")
+//  }
+//  else{
+//     console.log("user is not logged in. ")
+//  }
+
+// if(userloggedin.length===0){
+// console.log("Array is empty")
+// }
+// else {
+//     console.log("array is not empty")
+// }
+
+// const object={}
+// if(Object.keys(object).length===0){
+//     console.log("object is empty")
+// }
+
+//------ Nullish Coalescing Operator (??): null undefined-------
+
+// let a;
+// a =5 ?? 10
+// console.log(a)
+// a=5??15
+// console.log(a)
+// a=10??15
+// console.log(a)
+// a=null??155
+// console.log(a)
+// a=undefined ?? 20
+// console.log(a)
+
+// -----------Terniary Operator-----------
+
+// condition ? true(statement) : false(statement) example below
+
+const sweetprice=500
+sweetprice>=500 ? console.log("statement is true") : console.log("statement is false")

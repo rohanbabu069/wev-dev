b779a584c577fcc93b137bbf462a2ce1cb6a1557
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2025-11-06 13:00:41 +0530

    Learn how to use filter()function

---
+++ filter.js
@@ -0,0 +1,16 @@
+// ---------------filter()------------------
+
+// const arr=[1,2,3,4,5,6,7,8,9]
+// const newarr=arr.filter((item) => (item > 5))
+// console.log(newarr)
+// filter() function item return krta h but forEach() or loop does not return any value but dono ek ki trah se use hote h
+
+
+// const emptyarr=[]
+// const arr=[1,2,3,4,5,6,7,8,9]
+// arr.forEach( (item)=>{
+//     if(item>5){
+//         return emptyarr.push(item)
+//     }
+// })
+// console.log(emptyarr)

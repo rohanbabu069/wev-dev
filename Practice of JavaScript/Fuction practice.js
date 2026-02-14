c9ba666283780718c04a4c3bf38f22a574115cfa
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2026-02-15 05:07:42 +0530

    start practicing of javascript

---
+++ Practice of JavaScript/Fuction practice.js
@@ -0,0 +1,7 @@
+/* How to write JavaScript function ?
+Its just Simple : write function keyword then function name use (){} after that write code in {} then call the function   */
+
+function func(){
+    console.log("Hello World")
+}
+func()
---
+++ Practice of JavaScript/object.js
@@ -0,0 +1,9 @@
+// how to creatte object in javascript 
+
+/* It is very simple use {} and store it in a variable and store value in it in the form of key : value pare. */
+
+const obj1 = {
+    a: 'Rohan',
+    b: 'babu'
+}
+console.log(obj1)

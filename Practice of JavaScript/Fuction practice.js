115cafc126ffc3e5ccdc8c024447d1d81a08cfb0
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2026-02-24 19:10:11 +0530

    practicing and learning

--- Practice of JavaScript/Fuction practice.js
+++ Practice of JavaScript/Fuction practice.js
@@ -1,7 +1,39 @@
 /* How to write JavaScript function ?
 Its just Simple : write function keyword then function name use (){} after that write code in {} then call the function   */
 
-function func(){
-    console.log("Hello World")
+
+/*Below exammple is shown the asynconouse nature of javascript  */
+
+
+// function func(){
+//     console.log("Hello World")
+// }
+// func()
+// console.log('hello Rohan')
+// console.log('hello babu')
+
+// setTimeout(() => {
+//     console.log('Dishu')
+// },0)
+
+// setTimeout(() => {
+//     console.log('Byeeee')
+// })
+// console.log('hello Chandravanshi')
+
+
+// ------------------callback function-----------------------
+
+
+function saybye(){
+    console.log('Byeeeee Baby')
+    console.log('123445')
 }
-func()
+
+function greet(a,sayBye){
+console.log('hello ' +a);
+console.log('string')
+sayBye()
+}
+
+greet('rohan', saybye)
--- Practice of JavaScript/object.js
+++ Practice of JavaScript/object.js
@@ -2,8 +2,20 @@
 
 /* It is very simple use {} and store it in a variable and store value in it in the form of key : value pare. */
 
-const obj1 = {
-    a: 'Rohan',
-    b: 'babu'
+// const obj1 = {
+//     a: 'Rohan',
+//     b: 'babu'
+// }
+// console.log(obj1)
+
+// function a(){
+//     console.log('hello world')
+// }
+// a()
+
+let Sum=function sum(){
+    let a=23
+    let b=43
+    return c=a+b
 }
-console.log(obj1)
+console.log(Sum())

5bc653b91a21cc67345751c3f6c1bf7c392995ef
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2026-02-27 14:48:33 +0530

    keep practicing some more topic of javascript

---
+++ DSA with JavaScript/01_Traversing_Array.js
@@ -0,0 +1,24 @@
+// let a=[23,43,21,56,76,89,98]
+// for(const value in a){
+//     console.log(value)
+// }
+//------------------------------------------------------------
+// for(const val of a){
+//     console.log(val)
+// }
+
+// for(let val in a){
+//     console.log(val)
+// }
+// for(let val of a){
+//     console.log(val)
+// }
+
+//------------------------ traversing of Array -----------------------------------
+
+/* Traversing of Array with the help of for Loop */
+
+let a=[23,43,21,56,76,89,98]
+for (let i=0; i<a.length;i++){
+    console.log(a[i])
+}
---
+++ Practice of JavaScript/Loop.js
@@ -0,0 +1,66 @@
+// console.log('hello')
+
+//---------------------------------- for Loop ----------------------------
+
+// let a=1
+// for(let b=0; b<10; b++){
+//     console.log(b)
+// }
+
+// for( let i=1;i<=10;i++
+// ){
+//     let c= i*2
+//     console.log(c)
+// }
+
+// for(let i=1;i<=10;i++){
+//     let c=i-2
+//     console.log(c)
+// }
+
+// let num=5
+// for(let i=num;i>0;i--){
+//     let c=num
+//     let d=num-i
+//     let e=c*d
+//     console.log(e)
+// }
+
+//------------------------------forin loop------------------------------------
+
+// let obj1={
+//     a:'hello',
+//     b:12,
+//     c:true
+// }
+// for (const key in obj1) { 
+//     // const element = obj1[key];
+//     // console.log(key,element)
+//     console.log(key)
+// }
+
+// let object={
+//     a:'rohan',
+//     b:23,
+//     c:45
+// }
+// for (const value in object) {
+//     const element = object[value];
+//     console.log(element)
+// }
+
+// let object={
+//     a:'rohan',
+//     b:23,
+//     c:45
+// }
+// for(let a in object){
+//     console.log(a)
+// }
+
+//---------------------------------for of loop----------------------------------------
+
+let a='Rohan'
+for (const element of a) {
+    console.log(element)
+}

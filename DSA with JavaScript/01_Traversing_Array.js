b046f9a67592df934df06cfc43e1ae90c8641d67
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2026-02-27 15:08:36 +0530

    code for Forward and Backward Traversing in javascript

--- DSA with JavaScript/01_Traversing_Array.js
+++ DSA with JavaScript/01_Traversing_Array.js
@@ -16,9 +16,16 @@
 
 //------------------------ traversing of Array -----------------------------------
 
-/* Traversing of Array with the help of for Loop */
+/* Forward Traversing of Array with the help of for Loop */
+
+// let a=[23,43,21,56,76,89,98]
+// for (let i=0; i<a.length;i++){
+//     console.log(a[i])
+// }
+
+//Bacward Traversing of Array with the help of for Loop
 
 let a=[23,43,21,56,76,89,98]
-for (let i=0; i<a.length;i++){
+for(let i=a.length-1;i>=0;i--){
     console.log(a[i])
 }

ca29b64cebd4ba68be012243c6568c2b9d620309
rohanbabu069 <159506950+rohanbabu069@users.noreply.github.com>
2026-03-01 22:01:14 +0530

    practice of basic

---
+++ DSA with JavaScript/02_insertion_Array.js
@@ -0,0 +1,35 @@
+//---------------------------Insertion in Array---------------------------
+
+
+// let arr=[10,20,30,40,50,70,80,90]
+// let position=5
+// let newEl=60
+// console.log(arr)
+// for(let i=arr.length-1;i>=0;i--){
+//     // console.log(arr[i])
+//     if(i>=position){
+//         arr[i+1]=arr[i]
+//         if(i==position){
+//             arr[i]=newEl
+           
+//         }
+//     }
+// }
+// console.log(arr)
+
+// let arr=[10,20,30,40,50,70,80,90]
+// let position=4
+// let el=45
+// for(let i=arr.length-1;i>=0;i--){
+//     // console.log(arr[i])
+//     if(i>=position){
+//         arr[i+1]=arr[i]
+//         // console.log(arr[i])
+//         if(i==position){
+//             arr[i]=el
+//         }
+//     }
+// }
+// console.log(arr)
+// console.log(arr)
+
